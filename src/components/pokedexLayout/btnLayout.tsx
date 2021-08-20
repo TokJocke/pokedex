b6f00f264b9b2891b6d1c5522164c2f62ca8535a
Ossian Gren <Ossi_gren@hotmail.com>
2021-08-20 13:53:57 +0200

    absolut-sista

--- src/components/pokedexLayout/btnLayout.tsx
+++ src/components/pokedexLayout/btnLayout.tsx
@@ -1,119 +1,112 @@
-import * as React from 'react';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Button from '@material-ui/core/Button';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-import { Link } from '@material-ui/core';
-import HomeSharpIcon from '@material-ui/icons/HomeSharp';
-import AccountBalanceSharpIcon from '@material-ui/icons/AccountBalanceSharp';
-import FilterHdrSharpIcon from '@material-ui/icons/FilterHdrSharp';
-import BeachAccessSharpIcon from '@material-ui/icons/BeachAccessSharp';
-import { PokeContext, RegionDetails } from '../../context/pokeContext';
-import { CSSProperties } from 'react';
+import * as React from "react";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import { Link } from "@material-ui/core";
+import HomeSharpIcon from "@material-ui/icons/HomeSharp";
+import AccountBalanceSharpIcon from "@material-ui/icons/AccountBalanceSharp";
+import FilterHdrSharpIcon from "@material-ui/icons/FilterHdrSharp";
+import BeachAccessSharpIcon from "@material-ui/icons/BeachAccessSharp";
+import { PokeContext, RegionDetails } from "../../context/pokeContext";
+import { CSSProperties } from "react";
 
-interface Props {
-}
-
+interface Props {}
 
 export default class BtnLayout extends React.Component {
-    constructor(props: Props) {
-        super(props)
-    }
+  constructor(props: Props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <div style={btnFlexbox}>
+        <div style={btnBlue}>
+          <ButtonGroup>
+            <Button href="/" startIcon={<HomeSharpIcon />} style={blueButton} />
+            <Button startIcon={<FilterHdrSharpIcon />} style={blueButton} />
+            <Button startIcon={<BeachAccessSharpIcon />} style={blueButton} />
 
- 
- 
-
-    render() {
-
-        return (
-        <div style = {btnFlexbox}>
-
-            <div style = { btnBlue }>
-            <ButtonGroup>
-                <Button 
-                    onClick={() => console.log(this.context.pokemonData)}
-                    startIcon={<HomeSharpIcon/>} style={blueButton} 
-                    />
-                <Button 
-                    startIcon={<FilterHdrSharpIcon/>} style={blueButton} 
-                />
-                <Button 
-                    startIcon={<BeachAccessSharpIcon/>} style={blueButton} 
-                />
-                
-                <Button 
-                    startIcon={<AccountBalanceSharpIcon/>} style={blueButton} 
-                />
-                <Button 
-                    target="_blank" 
-                    href="https://github.com/TokJocke/pokedex" 
-                    startIcon={<GitHubIcon/>} 
-                    style={blueButton} 
-                />
-            </ButtonGroup>
-            </div>
-
-
-            <div style = { btnWhite }>
-            <ButtonGroup>
-            <Button             
-                onClick={ () => this.context.pokemonData? console.log("Nothing happend") : this.context.pokemonFuncs.setRegion(+1) } 
-                startIcon={<ArrowDownwardIcon/>} style={whiteButton}
+            <Button
+              startIcon={<AccountBalanceSharpIcon />}
+              style={blueButton}
             />
-            <Button 
-                onClick={ () => this.context.pokemonData? console.log("Nothing happend") : this.context.pokemonFuncs.setRegion(-1) } 
-
-/*                 onClick={ () => this.context.pokemonFuncs.setRegion(-1) }
- */                startIcon={<ArrowUpwardIcon/>} style={whiteButton}
+            <Button
+              target="_blank"
+              href="https://github.com/TokJocke/pokedex"
+              startIcon={<GitHubIcon />}
+              style={blueButton}
             />
-            </ButtonGroup>
-            </div>
+          </ButtonGroup>
         </div>
-    )  
-    }
+
+        <div style={btnWhite}>
+          <ButtonGroup>
+            <Button
+              onClick={() =>
+                this.context.pokemonData
+                  ? console.log("Nothing happend")
+                  : this.context.pokemonFuncs.setRegion(+1)
+              }
+              startIcon={<ArrowDownwardIcon />}
+              style={whiteButton}
+            />
+            <Button
+              onClick={() =>
+                this.context.pokemonData
+                  ? console.log("Nothing happend")
+                  : this.context.pokemonFuncs.setRegion(-1)
+              }
+              /*                 onClick={ () => this.context.pokemonFuncs.setRegion(-1) }
+               */ startIcon={<ArrowUpwardIcon />}
+              style={whiteButton}
+            />
+          </ButtonGroup>
+        </div>
+      </div>
+    );
+  }
 }
-      
-BtnLayout.contextType = PokeContext
+
+BtnLayout.contextType = PokeContext;
 
 const blueButton: CSSProperties = {
-    backgroundColor: "#28ABFD",
-    width: "50px",
-    height: "40px",
-    paddingRight: "4px",
-    margin: "0px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    border: "1px solid black"
-}
+  backgroundColor: "#28ABFD",
+  width: "50px",
+  height: "40px",
+  paddingRight: "4px",
+  margin: "0px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "1px solid black",
+};
 
 const whiteButton: CSSProperties = {
-    backgroundColor: "white",
-    width: "45px",
-    height: "35px",
-    paddingRight: "4px",
-    margin: "0px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    border: "1px solid black"
-}
+  backgroundColor: "white",
+  width: "45px",
+  height: "35px",
+  paddingRight: "4px",
+  margin: "0px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "1px solid black",
+};
 
 const btnBlue: CSSProperties = {
-    padding: "0em",
-    
-}
+  padding: "0em",
+};
 
 const btnWhite: CSSProperties = {
-    paddingTop: "2em",
-}
+  paddingTop: "2em",
+};
 
 const btnFlexbox: CSSProperties = {
-    display: "flex",
-    flexDirection: "column",
-    padding: "1em",
-    width: "100%",
-    margin: "0"
-}
+  display: "flex",
+  flexDirection: "column",
+  padding: "1em",
+  width: "100%",
+  margin: "0",
+};
